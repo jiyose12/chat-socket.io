dfdd7ca09ad4079ef0982c05cb5bde000c376483
jiyose <jiyose@outlook.com>
2020-12-10 20:13:52 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('disconnect message', "User has disconnected");
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
